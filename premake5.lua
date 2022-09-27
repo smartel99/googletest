d5b73e18f0dfa8904d8914c8abaa4704ae1cb025
Samuel Martel <martelsamuel00@gmail.com>
2022-09-27 11:06:41 -0400

    Add premake script

---
+++ premake5.lua
@@ -0,0 +1,36 @@
+-- LuaFormatter off
+project "gtest"
+    kind "StaticLib"
+    language "C++"
+
+    -- local outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+    targetdir("bin/"..outputdir.."/%{prj.name}")
+    objdir("bin-int/"..outputdir.."/%{prj.name}")
+
+    includedirs {
+        "googletest",
+        "googletest/include",
+        "googletest/include/gtest",
+    }
+
+    files
+    {
+        "googletest/**.h",
+        "googletest/**.hpp",
+        "googletest/src/gtest-all.cc"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        staticruntime "On"
+
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+-- LuaFormatter on
